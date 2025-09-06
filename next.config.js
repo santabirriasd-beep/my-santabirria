8745b2c3b9208d4932014cb269c9a5f00b30210e
santabirriasd-beep <santabirriasd@gmail.com>
2025-09-06 18:34:40 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true
+module.exports = {
+  reactStrictMode: true,
+  output: "export",
+  images: { unoptimized: true }
 };
-module.exports = nextConfig;
