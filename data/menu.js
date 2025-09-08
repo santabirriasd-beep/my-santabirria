e75837eec88ac061372ca55dd9b80391d1db42e8
santabirriasd-beep <santabirriasd@gmail.com>
2025-09-07 21:40:41 -0500

    Update menu.js

--- data/menu.js
+++ data/menu.js
@@ -1,104 +1,111 @@
 const menuData = {
   destacados: [
     {
-       id: "quesabirrias",
-  name: "Quesabirrias",
-  desc: "La estrella de la casa, cuatro tortillas bañadas en la grasa de la birria, su porción de proteína y consomé para dipear.",
-  price: 7.99,
-  image: "/platos/ChatGPT Image 6 sept 2025, 11_49_58 p.m..png" },
+      id: "quesabirrias",
+      name: "Quesabirrias",
+      desc: "La estrella de la casa, cuatro tortillas bañadas en la grasa de la birria, su porción de proteína y consomé para dipear.",
+      price: 7.99,
+      image: "/platos/ChatGPT Image 6 sept 2025, 11_49_58 p.m..png"
+    },
     {
       id: "En1",
       name: "Enchiladas",
       desc: "Tortillas rellenas de tu proteína preferida, queso rallado, salsa agria, aguacate y lechuga acompañada de una salsa roja que te dejara con ganas de más.",
-      price: "8.90",
+      price: 8.9,
       image: "/platos/enchiladas.jpg"
     },
-     {
+    {
       id: "EnZu1",
       name: "Enchiladas Suizas",
       desc: "Tortillas de maíz rellenas de tu proteína preferida, bañadas en una cremosa salsa de jitomate con un toque de crema y gratinadas con queso fundido, servidas con guarniciones frescas.",
-      price: "8.99",
-      image: "/platos/enchiladas suizas.jpg" },
-  
-     {
+      price: 8.99,
+      image: "/platos/enchiladas suizas.jpg"
+    },
+    {
       id: "Bt1",
       name: "Burrito Tradicional",
       desc: "Tortilla de harina grande, birria jugosa, frijoles, queso derretido, pico de gallo, crema y guacamole; sellado a la plancha para darle ese dorado perfecto.",
-      price: "8.50",
+      price: 8.5,
       image: "/platos/burrito tradicional.jpg"
     },
     {
       id: "Tst1",
       name: "Tostada de Tinga",
       desc: "Crujientes tortillas de maíz cubierta con pollo deshebrado en salsa de jitomate, frijoles refritos y cebolla en escabeche, coronada con crema agria, lechuga fresca, queso rallado y aguacate.",
-      price: "3.99",
-      image: "/platos/tingas.jpg" },
-     {
+      price: 3.99,
+      image: "/platos/tingas.jpg"
+    },
+    {
       id: "AgV1",
       name: "Aguachile Verde",
       desc: "Camarón “desnaturalizado” en limón, chile jalapeño, pepino y cebolla; equilibrio perfecto entre acidez y picante, acompañado de unas tostadas de maíz.",
-      price: "6.75",
-      image: "/platos/aguachile verde.jpg"},
-     {
+      price: 6.75,
+      image: "/platos/aguachile verde.jpg"
+    },
+    {
       id: "AgR1",
       name: "Aguachile Rojo",
       desc: "Camarón “desnaturalizado” en limón, chile guajillo y cebolla en escabeche; para los amantes de lo intenso, muy picante, acompañado de unas tostadas de maíz.",
-      price: "6.99",
-      image: "/platos/ChatGPT Image 7 sept 2025, 09_59_40 a.m..png"},
+      price: 6.99,
+      image: "/platos/ChatGPT Image 7 sept 2025, 09_59_40 a.m..png"
+    },
     {
       id: "AgA1",
       name: "Aguachile Amarillo",
       desc: "Camarón “desnaturalizado” en limón, chile habanero, mango, maracuyá y cebolla en escabeche; para los amantes de lo ácido, acompañado de unas tostadas de maíz.",
-      price: "6.75",
-      image: "/platos/ChatGPT Image 7 sept 2025, 10_49_13 a.m..png"},
+      price: 6.75,
+      image: "/platos/ChatGPT Image 7 sept 2025, 10_49_13 a.m..png"
+    },
     {
       id: "CbA1",
       name: "Birria Cone",
       desc: "Tortilla de trigo dorada y crujiente en forma de cono, rellena de frijoles refritos, crema agria y birria deshebrada, queso fundido y coronada con guacamole fresco. Una creación Street Food con el sello único de Santa Birria.",
-      price: "7.50",
-      image: "/platos/ChatGPT Image 7 sept 2025, 10_31_50 a.m..png"},
+      price: 7.5,
+      image: "/platos/ChatGPT Image 7 sept 2025, 10_31_50 a.m..png"
+    },
     {
-      id: "CbA1",
+      id: "ChC1",
       name: "Chili Cone",
       desc: "Un clásico mexicano lleno de sabor, chili de res bien sazonado con frijoles, servido junto a aguacate y totopos artesanales para disfrutar su crocancia en cada bocado.",
-      price: "7.50",
-      image: "/platos/ChatGPT Image 7 sept 2025, 04_43_40 p.m..png"},
-     {
+      price: 7.5,
+      image: "/platos/ChatGPT Image 7 sept 2025, 04_43_40 p.m..png"
+    },
+    {
       id: "Fls1",
       name: "Flautas",
       desc: "¡Que se arme la orquesta!! Tres deliciosas flautas crocantes rellenas de pollo, papa y carne punzada, se acompaña con lechuga fresca, queso rallado y crema agria, la combinación que necesitabas, pero no sabias.",
-      price: "7.50",
-      image: "/platos/ChatGPT Image 7 sept 2025, 04_22_03 p.m..png"},
-      
+      price: 7.5,
+      image: "/platos/ChatGPT Image 7 sept 2025, 04_22_03 p.m..png"
+    }
   ],
   combos: [
     {
       id: "c1",
       name: "El Pecado Mortal",
       desc: "3 quesatacos + Agua de Jamaica + Totopos artesanales + Guarniciones de la casa.",
-      price: "6.50",
+      price: 6.5,
       image: "/platos/ChatGPT Image 7 sept 2025, 07_46_55 p.m..png"
     },
     {
       id: "c2",
       name: "La Última Cena",
       desc: "Burrito clásico + Horchata + Caldo de Birria + Flan Napolitano + Guarniciones de la casa",
-      price: "14.99",
+      price: 14.99,
       image: "/platos/ChatGPT Image 7 sept 2025, 08_41_35 p.m..png"
     },
     {
       id: "c3",
       name: "Tres Plomasos",
       desc: "Enchiladas rojas + bebida + Totopos artesanales + Guarniciones de la casa.",
-      price: "8.50",
-      image: "/plato/ChatGPT Image 7 sept 2025, 09_04_38 p.m..png" },
-      {
-      id: "c3",
+      price: 8.5,
+      image: "/platos/ChatGPT Image 7 sept 2025, 09_04_38 p.m..png"
+    },
+    {
+      id: "c4",
       name: "El Forastero",
       desc: "2 Tingas de Pollo + 2 Tingas de Carne + 2 Tingas de Birria.",
-      price: "8.50",
-      image: "/placeholder.png" },
-    
+      price: 8.5,
+      image: "/placeholder.png"
     }
   ],
   bebidas: [
@@ -106,21 +113,21 @@ const menuData = {
       id: "b1",
       name: "Agua de Jamaica",
       desc: "Refrescante, menos azúcar.",
-      price: "1.50",
+      price: 1.5,
       image: "/placeholder.png"
     },
     {
       id: "b2",
       name: "Horchata",
       desc: "Arroz, canela, vainilla.",
-      price: "1.80",
+      price: 1.8,
       image: "/placeholder.png"
     },
     {
       id: "b3",
       name: "Limonada Rosada",
       desc: "Cítrica y ligera.",
-      price: "1.80",
+      price: 1.8,
       image: "/placeholder.png"
     }
   ]
